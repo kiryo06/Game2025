d03dcdc330511318357ffccdbe822f1c34deb0cb
kuru <166367588+kiryo06@users.noreply.github.com>
2025-07-09 16:47:19 +0900

    up

--- BLUE-PROTOCOL(reference)/Game/Boss.cpp
+++ BLUE-PROTOCOL(reference)/Game/Boss.cpp
@@ -2,24 +2,45 @@
 #include "Player.h"
 namespace
 {
-	constexpr const int CloseFrame = 60 * 2;	// 近距離のフレーム数
-	constexpr const int FarFrame = 60 * 2;		// 遠距離のフレーム数
-	constexpr const float BossRad  = { 1000.0f};
-	constexpr const float Close = {8000.0f};
-	constexpr const float Far = {10000.0f};
-	constexpr const VECTOR BossPos = { 0.0f, 0.0f, -1000.0f };
+	constexpr	const	int		CloseFrame		= 60 * 2;					// 近距離のフレーム数
+	constexpr	const	int		FarFrame		= 60 * 3;					// 遠距離のフレーム数
+	constexpr	const	int		TargetFrame		= 60 * 4;					// ターゲットのフレーム数
+	constexpr	const	int		TActionFrame	= 60 * 2;					// ターゲットのフレーム数
+	constexpr	const	int		FActionFrame	= 60 * 2;					// ターゲットのフレーム数
+	constexpr	const	int		CActionFrame	= 60 * 2;					// ターゲットのフレーム数
+	constexpr	const	float	BossRad			= {  1000.0f };				// ボスの半径
+	constexpr	const	float	Close			= {  4000.0f };				// 近距離かどうかの判定距離
+	constexpr	const	float	Far				= {  7000.0f };				// 遠距離かどうかの判定距離
+	constexpr	const	float	Target			= { 10000.0f };				// ターゲットできる距離
+	constexpr	const	float	MoveUpSpeed		= {	   10.0f };				// 近づく速度
+	constexpr	const	float	MoveDownSpeed	= {     5.0f };				// 離れる速度
+	constexpr	const	VECTOR	BossPos			= { 0.0f, 0.0f, -1000.0f };	// ボスの初期位置
+}
+
+int GetRandom(int min, int max)
+{
+	return min + (int)(rand() * (max - min + 1.0) / (1.0 + RAND_MAX));
 }
 
 Boss::Boss():
 	m_model(-1),
 	m_frameCountClose(0),
 	m_frameCountFar(0),
+	m_frameCountTarget(0),
+	m_frameCountTAction(0),
+	m_frameCountFAction(0),
+	m_frameCountCAction(0),
 	m_rad(0.0f),
+	m_playerRad(0.0f),
 	m_pos(0.0f, 0.0f, 0.0f),
+	m_playerPos(0.0f, 0.0f, 0.0f),
 	m_close(false),
 	m_far(false),
-	m_playerRad(0.0f),
-	m_playerPos(0.0f,0.0f,0.0f)
+	m_target(false),
+	m_moveUp(false),
+	m_moveDown(false),
+	m_meleeAttack(false),
+	m_rangedAttack(false)
 {
 }
 
@@ -36,15 +57,32 @@ void Boss::Init()
 
 void Boss::Update(Player* m_pPlayer)
 {
+	// プレイヤーの位置を受け取る
 	m_playerPos = m_pPlayer->GetPos();
+	// プレイヤーの当たり判定の半径を受け取る
 	m_playerRad = m_pPlayer->GetColRadius();
+	// ボスの位置からプレイヤーの位置引いた値を入れる
+	float distX = m_pos.x - m_playerPos.x;
+	float distY = m_pos.y - m_playerPos.y;
+	float distZ = m_pos.z - m_playerPos.z;
+	// ボスとプレイヤーの距離を求める
+	float dist = (distX * distX) + (distY * distY) + (distZ * distZ);
+	dist = sqrtf(dist);
 	// ボスが攻撃するために必要な距離の条件
-	CloseDistance();
-	FarDistance();
+	CloseDistance(dist);
+	FarDistance(dist);
+	TargetDistance(dist);
+
+	// 距離に合わせて何をするかを判断する
+	DecideAction();
+	// ボスの移動
+	MoveBoss();
 
 	// ボスの攻撃
+	MeleeAttack();
+	RangedAttack();
 
-
+	// ボスの向いている向きを変える
 	VECTOR das;
 	das = m_pPlayer->GetPos();
 	das = VSub(das, m_pos);
@@ -71,42 +109,50 @@ void Boss::Draw()
 	// ボスのモデルを描画
 	MV1DrawModel(m_model);
 
-	// 当たり判定のデバック表示
 #ifdef _DEBUG
+	// ボスの当たり判定
+	DrawCapsule3D(m_pos, m_pos, BossRad, 16, 0xffff00, 0xffff00, false);
 	// 近距離のターゲット
 	DrawCapsule3D(m_pos, m_pos, Close,16,0xffff00,0xffff00,false);
 	// 遠距離のターゲット
 	DrawCapsule3D(m_pos, m_pos, Far,16,0xffff00,0xffff00,false);
-	printf("近い:%d遠い:%d\n", m_frameCountClose, m_frameCountFar);
-	if (m_close && !m_far)
+	// ターゲットできる範囲
+	DrawCapsule3D(m_pos, m_pos, Target, 16, 0xffff00, 0xffff00, false);
+
+	printf("近い:%d遠い:%dターゲット:%d\n", m_frameCountClose, m_frameCountFar,m_frameCountTarget);
+	printf("移動:%d\n", m_frameCountTAction);
+	if (m_close)
 	{
-		DrawFormatString(20, 100, 0xffffff, "近距離\n");
+		DrawFormatString(20, 150, 0xffffff, "近距離\n");
 	}
-	if (m_far && !m_close)
+	if (m_far)
 	{
-		DrawFormatString(20, 100, 0xffffff, "遠距離\n");
+		DrawFormatString(20, 166, 0xffffff, "遠距離\n");
 	}
-	if (m_close && m_far)
+	if (m_target)
 	{
-		DrawFormatString(20, 100, 0xff00ff, "両方ついている\n");
+		DrawFormatString(20, 182, 0x00ff00, "ターゲット中\n");
 	}
-	if (!m_close && !m_far)
+	if(!m_close && !m_far && !m_target)
 	{
-		DrawFormatString(20, 100, 0xff00ff, "どちらでもない\n");
+		DrawFormatString(20, 198, 0xff0000, "範囲外\n");
 	}
 
+	if (m_moveUp|| m_moveDown)
+	{
+		DrawFormatString(20, 204, 0x00ff00, "移動中\n");
+	}
+
+	if (!m_moveUp && !m_moveDown)
+	{
+		DrawFormatString(20, 204, 0x00ff00, "移動不可\n");
+	}
 #endif // _DEBUG
 }
 
-void Boss::CloseDistance()
+void Boss::CloseDistance(float closeDist)
 {
-	float distX = m_pos.x - m_playerPos.x;
-	float distY = m_pos.y - m_playerPos.y;
-	float distZ = m_pos.z - m_playerPos.z;
-
-	float dist = (distX * distX) + (distY * distY) + (distZ * distZ);
-	dist = sqrtf(dist);
-	if ((dist) < (m_playerRad + Close))
+	if ((closeDist) < (m_playerRad + Close))
 	{
 		m_close = true;
 		m_frameCountClose = 0;
@@ -131,15 +177,9 @@ void Boss::CloseDistance()
 	}
 }
 
-void Boss::FarDistance()
+void Boss::FarDistance(float farDist)
 {
-	float distX = m_pos.x - m_playerPos.x;
-	float distY = m_pos.y - m_playerPos.y;
-	float distZ = m_pos.z - m_playerPos.z;
-
-	float dist = (distX * distX) + (distY * distY) + (distZ * distZ);
-	dist = sqrtf(dist);
-	if ((dist) < (m_playerRad + Far))
+	if ((farDist) < (m_playerRad + Far))
 	{
 		if (!m_close)
 		{
@@ -163,10 +203,173 @@ void Boss::FarDistance()
 			{
 				// n秒経過したら遠いフラグを外す
 				m_far = false;
+				// ターゲットフラグを立てる
+				// ここで建てないと遠いフラグが立っている状態でターゲットの範囲を超えたときにバグが発生する
+				m_target = true;
 				m_frameCountFar = 0;
 				return;
 			}
 			m_frameCountFar++;
 		}
+	}
+}
+
+void Boss::TargetDistance(float targetDist)
+{
+	if ((targetDist) < (m_playerRad + Target))
+	{
+		if (!m_close&&!m_far)
+		{
+			// 遠いフラグが立っていない場合はターゲットフラグを立てる
+			m_target = true;
+			m_frameCountTarget = 0;
+		}
+		else
+		{
+			// 遠いフラグが立っている場合はターゲットフラグを外す
+			m_target = false;
+			m_frameCountTarget = 0;
+		}
+	}
+	else
+	{
+		// 範囲外ではあるがまだターゲットフラグを立てておく時間
+		if (m_target)
+		{
+			if (m_frameCountTarget == TargetFrame)
+			{
+				// n秒経過したらターゲットフラグを外す
+				m_target = false;
+				m_frameCountTarget = 0;
+				return;
+			}
+			m_frameCountTarget++;
+		}
+	}
+}
+
+void Boss::DecideAction()
+{
+	if (m_close)		// 近い範囲にいる場合
+	{
+		// 何秒かに一回処理を変える
+		if (m_frameCountCAction == CActionFrame)
+		{
+			// n秒経過したらターゲットフラグを外す
+			// ランダムで値を取得するためのもの
+			int randValue = GetRandom(1, 6);
+			if (randValue <= 3)	// 1/2の確率で近づいてくる
+			{
+				m_moveUp = true;
+			}
+			else				// 何もしない
+			{
+				m_moveUp = false;
+			}
+			m_frameCountCAction = 0;
+			return;
+		}
+		m_frameCountCAction++;
+	}
+	else if (m_far)			// 遠い範囲にいる場合
+	{
+		// 何秒かに一回処理を変える
+		if (m_frameCountFAction == FActionFrame)
+		{
+			// n秒経過したらターゲットフラグを外す
+			// ランダムで値を取得するためのもの
+			int randValue = GetRandom(1, 6);
+			if (randValue <= 3)	// 1/2の確率で近づいてくる
+			{
+				m_moveUp = true;
+			}
+			else				// 何もしない
+			{
+				m_moveUp = false;
+			}
+			m_frameCountFAction = 0;
+			return;
+		}
+		m_frameCountFAction++;
+	}
+	else if (m_target)			// ターゲットできる範囲にいる場合
+	{
+		// 何秒かに一回処理を変える
+		if (m_frameCountTAction == TActionFrame)
+		{
+			// n秒経過したらターゲットフラグを外す
+			// ランダムで値を取得するためのもの
+			int randValue = GetRandom(1, 6);
+			if (randValue <= 3)	// 1/2の確率で近づいてくる
+			{
+				m_moveUp = true;
+			}
+			else				// 何もしない
+			{
+				m_moveUp = false;
+			}
+			m_frameCountTAction = 0;
+			return;
+		}
+		m_frameCountTAction++;
+	}
+	else
+	{
+		m_moveUp = false;
+		m_moveDown = false;
+		m_meleeAttack = false;
+		m_rangedAttack = false;
+	}
+}
+
+void Boss::MoveBoss()
+{
+	if (m_moveUp)
+	{
+		// ボスの移動
+		VECTOR direction = VSub(m_playerPos, m_pos);
+		direction = VNorm(direction); // 正規化
+		m_pos = VAdd(m_pos, VScale(direction, MoveUpSpeed));
+	}
+	if (m_moveDown)
+	{
+		// ボスの移動
+		VECTOR direction = VSub(m_playerPos, m_pos);
+		direction = VNorm(direction); // 正規化
+		m_pos = VAdd(m_pos, VScale(direction, -MoveDownSpeed));
+	}
+}
+
+void Boss::MeleeAttack()
+{
+	// 近接攻撃の処理
+	if (m_meleeAttack)
+	{
+		//// 近接攻撃の処理を行う
+		//m_frameCountClose++;
+		//if (m_frameCountClose >= CloseFrame)
+		//{
+		//	// 攻撃の処理を行う
+		//	m_frameCountClose = 0; // フレームカウントをリセット
+		//	// 攻撃の処理を書く
+		//	// 例えば、プレイヤーにダメージを与えるなど
+		//}
+	}
+}
+
+void Boss::RangedAttack()
+{
+	// 遠隔攻撃の処理
+	if (m_rangedAttack)
+	{
+		//// 遠隔攻撃の処理を行う
+		//m_frameCountFar++;
+		//if (m_frameCountFar >= FarFrame)
+		//{
+		//	// 攻撃の処理を行う
+		//	m_frameCountFar = 0; // フレームカウントをリセット
+		//	// 攻撃の処理を書く
+		//	// 例えば、プレイヤーにダメージを与えるなど
+		//}
 	}
 }
--- BLUE-PROTOCOL(reference)/Game/Boss.h
+++ BLUE-PROTOCOL(reference)/Game/Boss.h
@@ -8,13 +8,21 @@ private:
 	int m_model;				// ボスのモデル
 	int m_frameCountClose;		// 近い場合のフレームをカウントする
 	int m_frameCountFar;		// 遠い場合のフレームをカウントする
+	int m_frameCountTarget;		// ターゲットできる場合のフレームをカウントする
+	int m_frameCountTAction;	// ターゲットしている場合次に行う処理の合計フレームを計測する
+	int m_frameCountFAction;	// 遠い場合次に行う処理の合計フレームを計測する
+	int m_frameCountCAction;	// 近い場合次に行う処理の合計フレームを計測する
 	float m_rad;				// ボスの半径
+	float m_playerRad;			// プレイヤーの半径
 	VECTOR m_pos;				// ボスの位置
+	VECTOR m_playerPos;			// プレイヤーの位置を取得するメンバー
 	bool m_close;				// 近いかどうかを判断する
 	bool m_far;					// 遠いかどうかを判断する
-
-	float m_playerRad;			// プレイヤーの半径
-	VECTOR m_playerPos;			// プレイヤーの位置を取得するメンバー
+	bool m_target;				// ターゲットできるかどうかを判断する
+	bool m_moveUp;				// 進む移動をするかどうかを判断する
+	bool m_moveDown;			// 戻る移動をするかどうかを判断する
+	bool m_meleeAttack;			// 近接攻撃をするかどうかを判断する
+	bool m_rangedAttack;		// 遠隔攻撃をするかどうかを判断する
 public:
 	Boss();
 	~Boss();
@@ -26,8 +34,18 @@ public:
 	void Draw();
 private:
 	// 近距離
-	void CloseDistance();
+	void CloseDistance(float closeDist);
 	// 遠距離
-	void FarDistance();
+	void FarDistance(float farDist);
+	// ターゲットできる範囲
+	void TargetDistance(float targetDist);
+	// 攻撃をするか移動をするかとどまるかを判断する
+	void DecideAction();
+	// ボスの移動
+	void MoveBoss();
+	// 近接攻撃
+	void MeleeAttack();
+	// 遠隔攻撃
+	void RangedAttack();
 };
 
