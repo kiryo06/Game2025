a102b8f4b288647fb57d88f723370c87308cfdb5
kuru <166367588+kiryo06@users.noreply.github.com>
2025-09-19 15:36:58 +0900

    up

--- winter/Game/Game/Player.h
+++ winter/Game/Game/Player.h
@@ -8,7 +8,23 @@ public:
 	void Update();
 	void Draw();
 private:
+	// モデル
+	int m_model;
+
+	// 向きを保存するためのもの
+	float m_rotY;
+	float m_getCameraAtan2;
+
+	float m_attackFrameCount;
+
+	float m_hp;
+	float m_attack;
+
+	// 位置
 	VECTOR m_pos;
-	VECTOR m_dir;
+	// ベクトル
+	VECTOR m_vec;
+
+	bool m_isAttacking;
 };
 
